bda1c427292f93e4de9085be237b80d3d76c02b1
Markus Rosenberg <markus.rosenberg@softwareone.com>
2023-06-01 15:27:50 +0200

    working game

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def main():
     clock = pygame.time.Clock()
     
     running = True
+    max_fps = 60
+    frame_counter = 0
     
     tile_amount = 25
     tile_gap = 2
     
+    obtained_ore = 0
+    
     ore_patch = OrePatch(3, 3)
     miner = Miner(3, 3)
-    belt = Belt(2, 3, Vec2(0, 1))
+    belt_buffer: list[Belt] = [Belt(2, 3, Vec2(0, 1)), Belt(2, 4, Vec2(0, 1)), Belt(2, 5, Vec2(1, 0))]
+    ore_buffer: list[Ore] = []
+    hub = Collector(3, 5, 3, 3)
 
     while running:
         for event in pygame.event.get():
@@ -23,33 +29,56 @@ def main():
                 running = False
             if event.type == pygame.VIDEORESIZE:
                 bigger_size = max(event.w, event.h)
-                bigger_size = 800 if bigger_size > 800 else bigger_size
+                if bigger_size > 800: bigger_size = 800
+                elif bigger_size < 400: bigger_size = 400
                 surface = pygame.display.set_mode((bigger_size, bigger_size), pygame.RESIZABLE)
 
-        surface_width = surface.get_width()
-        surface_height = surface.get_height()
-        tile_width = 1 if surface_width < tile_amount else round(surface_width / tile_amount)
-        tile_height = 1 if surface_height < tile_amount else round(surface_height / tile_amount)
+        surface_size = surface.get_width()
+        tile_size = 1 if surface_size < tile_amount else round(surface_size / tile_amount)
         
-        ore = miner.mine([ore_patch])
-        if ore is not None:
-            ore.x = 2
-            belt.move_ore(ore)
-            
+        
+        if frame_counter == 60:
+            ore = miner.mine([ore_patch])
+            if ore is not None:
+                ore_buffer.append(ore)
+        
+            for o in ore_buffer:
+                
+                if o.x == 3 and o.y == 3:
+                    o.x = 2
+                    o.locked = True
+                    
+                for b in belt_buffer:
+                    if o.locked: continue
+                    b.move_ore(o)
+                    
+                obtained_ore += hub.pickup(o, ore_buffer)
+        
+            print(obtained_ore)
         
         surface.fill(0x000000)
-        for x in range(0, surface_width, tile_width):
-            for y in range(0, surface_height, tile_height):
-                surface.fill(0x86c06c, Rect(x + tile_gap, y + tile_gap, tile_width - tile_gap, tile_height - tile_gap))
-                surface.fill(0x2f6951, Rect((ore_patch.x * tile_width) + tile_gap, (ore_patch.y * tile_height) + tile_gap, tile_width - tile_gap, tile_height - tile_gap))
-                surface.fill(0xffffff, Rect((miner.x * tile_width) + 4, (miner.y * tile_height) + 4, tile_width - 8, tile_height - 8))
-                surface.fill(0xff00ff, Rect((belt.x * tile_width) + 4, (belt.y * tile_height) + 4, tile_width - 8, tile_height - 8))
-                if ore is not None:
-                    surface.fill(0x000000, Rect((ore.x * tile_width) + 4, (ore.y * tile_height) + 4, tile_width - 8, tile_height - 8))
+        for x in range(0, surface_size, tile_size):
+            for y in range(0, surface_size, tile_size):
+                surface.fill(0x86c06c, Rect(x + tile_gap, y + tile_gap, tile_size - tile_gap, tile_size - tile_gap))
+                surface.fill(0x2f6951, Rect((ore_patch.x * tile_size) + tile_gap, (ore_patch.y * tile_size) + tile_gap, tile_size - tile_gap, tile_size - tile_gap))
+                
+                for b in belt_buffer:
+                    surface.fill(0xff00ff, Rect((b.x * tile_size) + 4, (b.y * tile_size) + 4, tile_size - 8, tile_size - 8))
+                for o in ore_buffer:
+                    surface.fill(0x000000, Rect((o.x * tile_size) + 4, (o.y * tile_size) + 4, tile_size - 8, tile_size - 8))
+                
+                surface.fill(0xffffff, Rect((miner.x * tile_size) + 4, (miner.y * tile_size) + 4, tile_size - 8, tile_size - 8))
+                surface.fill(0x00ffff, Rect((hub.x * tile_size) + 4, (hub.y * tile_size) + 4, (tile_size * hub.width) - 8, (tile_size * hub.height) - 8))
         
         pygame.display.update()
 
-        clock.tick(60)
+        for o in ore_buffer:
+            o.locked = False
+
+        frame_counter += 1
+        if frame_counter > max_fps:
+            frame_counter = 0
+        clock.tick(max_fps)
 
     pygame.quit()
     sys.exit()
@@ -65,10 +94,11 @@ class OrePatch:
         self.y = y
         
 class Ore:
-    def __init__(self, x: int, y: int, amount: int) -> None:
+    def __init__(self, x: float, y: float, amount: int, locked: bool = False) -> None:
         self.x = x
         self.y = y
         self.amount = amount
+        self.locked = locked
         
 class Miner:
     def __init__(self, x: int , y: int) -> None:
@@ -88,12 +118,27 @@ class Belt:
         #in coord/s
         self.speed = speed
         
-    def move_ore(self, ore: Ore):
-        if (ore.x != self.x or ore.y != self.y):
+    def move_ore(self, ore: Ore) -> None:
+        if ore.x != self.x or ore.y != self.y or ore.locked == True:
             return 
-        
+           
         ore.x += self.speed.x
         ore.y += self.speed.y
+        ore.locked = True
+        
+class Collector:
+    def __init__(self, x: int, y: int, width: int, height: int) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        
+    def pickup(self, ore: Ore, ore_buffer: list[Ore]) -> int:
+        if (not ore.x in range(self.x, self.x + self.width) or not ore.y in range(self.y, self.y + self.height)):
+            return 0
+        
+        ore_buffer.remove(ore)
+        return 1
 
 if __name__ == "__main__":
     main()
